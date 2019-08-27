4a8f17ed4945d2338a11419f7326cfaa8edec3a4
Tamohagan <niari.mohamed@yahoo.fr>
2019-08-27 20:54:46 +0200

    Suppression de Spring data

---
+++ amis-de-escalade/couche-webapp/src/test/java/org/couche/webapp/test/CommentaireTest.java
@@ -0,0 +1,11 @@
+package org.couche.webapp.test;
+
+import java.text.ParseException;
+
+public class CommentaireTest {
+
+	public static void main(String[] args) throws NullPointerException, ParseException {
+
+	}
+
+}
