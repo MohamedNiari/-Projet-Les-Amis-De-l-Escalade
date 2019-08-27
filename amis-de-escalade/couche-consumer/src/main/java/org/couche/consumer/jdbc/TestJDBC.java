ea8813f15d3214637c00436b2e19e59a4c54b74b
Tamohagan <niari.mohamed@yahoo.fr>
2019-08-28 01:44:12 +0200

    test de connexion à la base de donnee

---
+++ amis-de-escalade/couche-consumer/src/main/java/org/couche/consumer/jdbc/TestJDBC.java
@@ -0,0 +1,28 @@
+package org.couche.consumer.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJDBC {
+
+	public static void main(String[] args) {
+		
+		String url = "jdbc:mysql://localhost:3306/escalade?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+		String user = "openclassroom";
+		String pass = "openclassroom";
+		
+		System.out.println("Connexion à la base de donnée : " + url);
+		
+		try {
+			Connection myConn =
+					DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Connexion réussi!!!");
+
+	}
+
+}
